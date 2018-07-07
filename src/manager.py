9bac9ec7bf61db5e02369b3b8a6b427fb1b4ee47
kleberyarus1990 <yaruskuan1990@gmail.com>
2018-07-07 10:54:56 -0500

    modificacion de clase manager y read csv

--- src/manager.py
+++ src/manager.py
@@ -1,90 +1,187 @@
 import pandas as pd
 import numpy as np
 
-class Scenary():
-    def resampleData(data,period,timeColumn):
-        resampled = data.resample(period,on=timeColumn).mean() #Resample the data for given time period
+class CreateScenary():
+    def GroupingData(data,period,timeColumn):
+        resampled = data.resample(period,on=timeColumn).mean()  #data resampled with period
 
         resampled['Hora'] = resampled.index.time #Retrieve the hour
-        scene01 = resampled.groupby('Hora').mean()  # Create an average values for every time step of any day
-        #print(scene01)
-        return scene01
+        stage01 = resampled.groupby('Hora').mean()  # Create an average values for every time step of any day
+        #print(stage01)
+        return stage01
 
-    def resampleData1(data,period,timeColumn):
-        resampled = data.resample(period,on=timeColumn).mean() #Resample the data for given time period
-
-        resampled['Mes'] = resampled.index.time #Retrieve the hour
-        scene01 = resampled.groupby('Mes').mean()  # Create an average values for every time step of any day
-        #print(scene01)
-        return scene01
-
-
-
-
-    def scene1(data,steps,concentracion): #Build scenary with varaible to be estimated
+    def stage1(data, hist, variable):  # Build stage
         long = len(data.index)
-        scene = pd.DataFrame(index=data.index[steps:long],columns=range(0,steps+1))
-        for i in range (0,long-steps):
-            temp = data.ix[data.index[i:i + steps+1], concentracion].values
-            temp2 = np.transpose(temp)
-            scene.ix[scene.index[i]] = temp2
-        return scene
 
-    def scene2(data, steps, variable1,variable2):  #Build scenary with two variables
-        long = len(data.index)
-        scene = pd.DataFrame(index=data.index[steps:long], columns=range(0, 2*steps))
-        for i in range(0, long - steps):
-            temp = data.ix[data.index[i:i + steps], variable1].values
-            temp1=data.ix[data.index[i:i + steps], variable2].values
-            temp2 = np.ravel(np.column_stack((temp,temp1)))
-            temp3 = np.transpose(temp2)
-            scene.ix[scene.index[i]] = temp3
-        scene.ix[scene.index[0:long - steps], 2 * steps] = data.ix[data.index[steps:long], variable2]
-        return scene
+        stage = pd.DataFrame(index=data.index[hist:long], columns=range(0, hist+1))
+        a=list(stage)
+        head=list()
+        a.reverse()
+        #print(a)
+        for x in range(0,len(a)):
+            head.insert(0,variable+' -'+str(x))
+        stage = pd.DataFrame(index=data.index[hist:long], columns=head)
+        for i in range(0, long - hist):
+            aux = data.ix[data.index[i:i + hist+1], variable].values
+            aux2 = np.transpose(aux)
+            stage.ix[stage.index[i]] = aux2
 
-    def scene3(data, steps, variable1, variable2,variable3):  #Build scenary with three variables
-        long = len(data.index)
-        scene = pd.DataFrame(index=data.index[steps:long], columns=range(0, 3 * steps))
-        for i in range(0, long - steps):
-            temp = data.ix[data.index[i:i + steps], variable1].values
-            temp2 = data.ix[data.index[i:i + steps], variable2].values
-            temp3=data.ix[data.index[i:i + steps], variable3].values
-            temp4 = np.ravel(np.column_stack((temp,temp2,temp3)))
-            temp5 = np.transpose(temp4)
-            scene.ix[scene.index[i]] = temp5
-        scene.ix[scene.index[0:long - steps], 3 * steps] = data.ix[data.index[steps:long], variable3]
-        return scene
+        return stage
 
-    def scene4(data, steps, variable1, variable2, variable3, variable4):  #Build scenary with three variables
+
+
+
+
+    '''def stage1(data,hist,variable): #Build stage with varaible to be estimated
         long = len(data.index)
-        scene = pd.DataFrame(index=data.index[steps:long], columns=range(0, 4 * steps))
-        for i in range(0, long - steps):
-            temp = data.ix[data.index[i:i + steps], variable1].values
-            temp2 = data.ix[data.index[i:i + steps], variable2].values
-            temp3=data.ix[data.index[i:i + steps], variable3].values
-            temp4 = data.ix[data.index[i:i + steps], variable4].values
-            temp5 = np.ravel(np.column_stack((temp,temp2,temp3,temp4)))
-            temp6 = np.transpose(temp5)
-            scene.ix[scene.index[i]] = temp6
-        scene.ix[scene.index[0:long - steps], 4 * steps] = data.ix[data.index[steps:long], variable4]
-        return scene
-'''def scene1(data,steps,concentracion): #Build scenary with varaible to be estimated
+        stage = pd.DataFrame(index=data.index[hist:long],columns=range(0,hist+1))
+        for i in range (0,long-hist):
+            aux = data.ix[data.index[i:i + hist+1], variable].values
+            aux2 = np.transpose(aux)
+            stage.ix[stage.index[i]] = aux2
+        return stage'''
+
+    def stage2(data, hist, variable1,variable2):  #Build stage with two variables
         long = len(data.index)
+        stage = pd.DataFrame(index=data.index[hist:long], columns=range(0, 2*hist))
+        a = list(stage)
         #print(long)
-        scene = pd.DataFrame(index=data.index[steps:long],columns=range(0,steps))
-        #scene=scene.dropna()
-        #scene1=scene[0]
-        d={1: (concentracion +' '+'-5'), 2: (concentracion +' '+'-4'), 3: (concentracion +' '+'-3'), 4: (concentracion +' '+'-2'), 5: (concentracion +' '+'-1')}
-        scene.rename(columns=d,inplace=True)
-        print("aqui")
-        #print(scene.rename(index=str, columns={0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "e"}))
-        print(list(scene))
-        for i in range (0,long-steps):
-            temp = data.ix [data.index[i:i + steps], concentracion].values
-            #print(temp)
-            temp2 = np.transpose(temp)
-            #print(temp2)
-            scene.ix[scene.index[i]] = temp2
-            #print(scene)
+        head = list()
+        a.reverse()
+        cont = 0
+        # print(variable1)
+        variables = [variable1, variable2]
 
-        return scene'''
+        temp = int(len(a) / 2)
+        # print(temp)
+        x = temp
+        x1 = 0
+        # x1 = len(variables)
+
+        for i in range(1, temp + 1):
+            for j in range(1, len(variables) + 1):
+                #   if x1<=len(variables):
+                head.insert(cont, variables[x1] + ' -' + str(x))
+                cont = cont + 1
+                x1 = x1 + 1
+                if x1 >= len(variables):
+                    x1 = 0
+            x = int(x) - 1
+
+        #head.append('a')
+        #stage = pd.DataFrame(index=data.index[hist:long], columns=head)
+
+        #stage.set_axis(['a', 'b', 'c'], axis=1, inplace=True)
+
+        print(stage.columns)
+
+        for i in range(0, long - hist):
+            aux = data.ix[data.index[i:i + hist], variable1].values
+            aux1=data.ix[data.index[i:i + hist], variable2].values
+            aux2 = np.ravel(np.column_stack((aux,aux1)))
+            aux3 = np.transpose(aux2)
+            stage.ix[stage.index[i]] = aux3
+        print("una")
+        print(stage)
+        stage.ix[stage.index[0:long - hist], 2 * hist] = data.ix[data.index[hist:long+1], variable2]
+        print("dos")
+        print(stage)
+
+
+
+
+        return stage
+
+    '''def stage2(data, hist, variable1,variable2):  #Build stage with two variables
+        long = len(data.index)
+        stage = pd.DataFrame(index=data.index[hist:long], columns=range(0, 2*hist))
+        for i in range(0, long - hist):
+            aux = data.ix[data.index[i:i + hist], variable1].values
+            aux1=data.ix[data.index[i:i + hist], variable2].values
+            aux2 = np.ravel(np.column_stack((aux,aux1)))
+            aux3 = np.transpose(aux2)
+            stage.ix[stage.index[i]] = aux3
+        stage.ix[stage.index[0:long - hist], 2 * hist] = data.ix[data.index[hist:long], variable2]
+        return stage'''
+
+    def stage3(data, hist, variable1, variable2,variable3):  #Build stage with three variables
+        long = len(data.index)
+        stage = pd.DataFrame(index=data.index[hist:long], columns=range(0, 3 * hist))
+        a = list(stage)
+        # print(a)
+        head = list()
+        a.reverse()
+        cont = 0
+        # print(variable1)
+        variables = [variable1, variable2,variable3]
+        temp = int(len(a) / 3)
+        # print(temp)
+        x = temp
+        x1 = 0
+        # x1 = len(variables)
+
+        for i in range(1, temp + 1):
+            for j in range(1, len(variables) + 1):
+                #   if x1<=len(variables):
+                head.insert(cont, variables[x1] + ' -' + str(x))
+                cont = cont + 1
+                x1 = x1 + 1
+                if x1 >= len(variables):
+                    x1 = 0
+
+            x = int(x) - 1
+        stage = pd.DataFrame(index=data.index[hist:long], columns=head)
+        for i in range(0, long - hist):
+            aux = data.ix[data.index[i:i + hist], variable1].values
+            aux2 = data.ix[data.index[i:i + hist], variable2].values
+            aux3=data.ix[data.index[i:i + hist], variable3].values
+            aux4 = np.ravel(np.column_stack((aux,aux2,aux3)))
+            aux5 = np.transpose(aux4)
+            stage.ix[stage.index[i]] = aux5
+        #stage.ix[stage.index[0:long - hist], 3 * hist] = data.ix[data.index[hist:long], variable3]
+        return stage
+
+    def stage4(data, hist, variable1, variable2, variable3, variable4):  #Build stage with three variables
+        long = len(data.index)
+        stage = pd.DataFrame(index=data.index[hist:long], columns=range(0, 4 * hist))
+        a = list(stage)
+        # print(a)
+        head = list()
+        a.reverse()
+        cont = 0
+        # print(variable1)
+        variables = [variable1, variable2,variable3,variable4]
+
+        temp = int(len(a) / 4)
+        # print(temp)
+        x = temp
+        x1 = 0
+        # x1 = len(variables)
+
+        for i in range(1, temp + 1):
+            for j in range(1, len(variables) + 1):
+                #   if x1<=len(variables):
+                head.insert(cont, variables[x1] + ' -' + str(x))
+                cont = cont + 1
+                x1 = x1 + 1
+                if x1 >= len(variables):
+                    x1 = 0
+
+            x = int(x) - 1
+        stage = pd.DataFrame(index=data.index[hist:long], columns=head)
+        for i in range(0, long - hist):
+            aux = data.ix[data.index[i:i + hist], variable1].values
+            aux2 = data.ix[data.index[i:i + hist], variable2].values
+            aux3=data.ix[data.index[i:i + hist], variable3].values
+            aux4 = data.ix[data.index[i:i + hist], variable4].values
+            aux5 = np.ravel(np.column_stack((aux,aux2,aux3,aux4)))
+            aux6 = np.transpose(aux5)
+            stage.ix[stage.index[i]] = aux6
+        #stage.ix[stage.index[0:long - hist], 4 * hist] = data.ix[data.index[hist:long], variable4]
+        return stage
+
+    def GroupingData1(data, period, timeColumn):
+        resampled = data.resample(period, on=timeColumn).mean()  # Resample the data for given time period
+        resampled['Mes'] = resampled.index.time  # Retrieve the hour
+        stage01 = resampled.groupby('Mes').mean()  # Create an average values for every time step of any day
+        return stage01
--- src/read_csv.py
+++ src/read_csv.py
@@ -1,165 +1,143 @@
 import pandas as pd
-from manager import Scenary
+from manager import CreateScenary
 
-########################Parameters to be set by user##########################
-period='10min'      #Period of time to evaluated
-historyStep=10       #Number of time steps to go back
-#testPercentage=0.9  #Fraction of data to be taken as Teste data
-##############################################################################
+########################input parameters by user##########################
+print("Ingrese el intervalo de tiempo a evaluar")
+#interval=input()                    #period
+interval='60min'                  #period
 
-##################Parameters to set according with available##################
-#todelete = '_id'               #Mongodb index name to be deleted
-validdate='2017/05/01 00:00:00' #Date when valid date starts
+print("Ingrese el numero de medidas hacia atras")
+#history=int(input())                #history
+history=1              #history
+
+print("Loading....")
+
 dateindex='TIMESTAMP'                #Column where date is stored
 columntime='TIMESTAMP'               #Column Name containing the new resampled Time
 ##############################################################################
 
 ###############################Reading csv file###############################
-df=pd.read_csv('../Data/final.csv',sep=';') #read data from csv file
-#df=pd.read_csv('../Data/final.csv',encoding='latin-1', low_memory=False) #read data from csv file
-date_set = pd.to_datetime(df.TIMESTAMP, unit='s')
+df=pd.read_csv('../Data/final.csv',sep=';')         #read data from csv file
+date_set = pd.to_datetime(df.TIMESTAMP, unit='s')   #convert column timestamp to datatime
 #final=df[['WINDING_TEMP_AVE']]
-df['TIMESTAMP']=date_set
-final=df[['TIMESTAMP','MPU4_KW','EXH_INL_GAS_TEMP_AVE','EXH_CYL_GAS_TEMP_AVE','WINDING_TEMP_AVE']]
-#final.set_index('TIMESTAMP')
-#final=df
-#dfx=df
+df['TIMESTAMP']=date_set                            #assign to column to dataframe of timestamp converter
+final=df[['TIMESTAMP','MPU4_KW','EXH_INL_GAS_TEMP_AVE','EXH_CYL_GAS_TEMP_AVE','WINDING_TEMP_AVE']]  #select the variables in a new dataframe
 dfx=final
 #print(dfx.dtypes)
 ##############################################################################
 
-###########################Processing Data####################################
-#Procesisng data to eliminate innecesary data and calculating aditional data
-#dfx=Prepare.prepare(df,dateindex,validdate,temperature1,temperature2)
-##############################################################################
+dfx_train=CreateScenary.GroupingData(dfx,interval,columntime)
 
-########################Creating Test and Training Data#######################
-#test=dfx.sample(frac=testPercentage,random_state=1)
-#train = dfx.drop(test.index)
-#dfx_train=Scenary.resampleData(train,period,columntime)
-dfx_train=Scenary.resampleData(dfx,period,columntime)
-dfx_train1=Scenary.resampleData1(dfx,period,columntime)
-#dfx_test=Scenary.resampleData(test,period,columntime)
-#print("inicio")
-#print(dfx_train)
-#print("fin")
-##############################################################################
 
 ##########################Assembly posible scenaries##########################
-# Only concentration
-data01=Scenary.scene1(dfx_train,historyStep,'MPU4_KW')
+print("Creating Scenaries....")
+################ VARIABLE MPU4_KW ################
+data01=CreateScenary.stage1(dfx_train,history,'MPU4_KW')
 data01.to_csv('../Scenaries/MPU4_KW/MPU4_KW.csv')
 
-#data011=Scenary.scene1(dfx_train1,historyStep,'MPU4_KW')
-#data011.to_csv('../Scenaries/Method1/CO/MPU4_KWmes.csv')
-
-# Concentration and one variable
-data02=Scenary.scene2(dfx_train,historyStep,'EXH_CYL_GAS_TEMP_AVE','MPU4_KW')
+# ONE VARIABLE VS MPU4_KW
+data02=CreateScenary.stage2(dfx_train,history,'EXH_CYL_GAS_TEMP_AVE','MPU4_KW')
 data02.to_csv('../Scenaries/MPU4_KW/EXH_CYL_GAS_TEMP_AVE VS MPU4_KW.csv')
 
-data02=Scenary.scene2(dfx_train,historyStep,'WINDING_TEMP_AVE','MPU4_KW')
+data02=CreateScenary.stage2(dfx_train,history,'WINDING_TEMP_AVE','MPU4_KW')
 data02.to_csv('../Scenaries/MPU4_KW/WINDING_TEMP_AVE VS MPU4_KW.csv')
 
-data02=Scenary.scene2(dfx_train,historyStep,'EXH_INL_GAS_TEMP_AVE','MPU4_KW')
+data02=CreateScenary.stage2(dfx_train,history,'EXH_INL_GAS_TEMP_AVE','MPU4_KW')
 data02.to_csv('../Scenaries/MPU4_KW/EXH_INL_GAS_TEMP_AVE VS MPU4_KW.csv')
 
-
-#Concentration and two variables
-data03=Scenary.scene3(dfx_train,historyStep,'EXH_CYL_GAS_TEMP_AVE','WINDING_TEMP_AVE','MPU4_KW')
+# TWO VARIABLES VS MPU4_KW
+data03=CreateScenary.stage3(dfx_train,history,'EXH_CYL_GAS_TEMP_AVE','WINDING_TEMP_AVE','MPU4_KW')
 data03.to_csv('../Scenaries/MPU4_KW/EXH_CYL_GAS_TEMP_AVE WINDING_TEMP_AVE VS MPU4_KW.csv')
 
-data03=Scenary.scene3(dfx_train,historyStep,'EXH_CYL_GAS_TEMP_AVE','EXH_INL_GAS_TEMP_AVE','MPU4_KW')
+data03=CreateScenary.stage3(dfx_train,history,'EXH_CYL_GAS_TEMP_AVE','EXH_INL_GAS_TEMP_AVE','MPU4_KW')
 data03.to_csv('../Scenaries/MPU4_KW/EXH_CYL_GAS_TEMP_AVE EXH_INL_GAS_TEMP_AVE VS MPU4_KW.csv')
 
-#data03=Scenary.scene3(dfx_train,historyStep,'WINDING_TEMP_AVE','EXH_CYL_GAS_TEMP_AVE','MPU4_KW')
-#data03.to_csv('../Scenaries/Method1/CO/Escenario31.csv')
-
-#Concentration and three varaibles
-data04=Scenary.scene4(dfx_train,historyStep,'EXH_INL_GAS_TEMP_AVE','EXH_CYL_GAS_TEMP_AVE','WINDING_TEMP_AVE','MPU4_KW')
+# THREE VARIABLES VS MPU4_KW
+data04=CreateScenary.stage4(dfx_train,history,'EXH_INL_GAS_TEMP_AVE','EXH_CYL_GAS_TEMP_AVE','WINDING_TEMP_AVE','MPU4_KW')
 data04.to_csv('../Scenaries/MPU4_KW/EXH_INL_GAS_TEMP_AVE EXH_CYL_GAS_TEMP_AVE WINDING_TEMP_AVE VS MPU4_KW.csv')
 
 
-data01=Scenary.scene1(dfx_train,historyStep,'EXH_CYL_GAS_TEMP_AVE')
+data01=CreateScenary.stage1(dfx_train,history,'EXH_CYL_GAS_TEMP_AVE')
 data01.to_csv('../Scenaries/EXH_CYL_GAS_TEMP_AVE/EXH_CYL_GAS_TEMP_AVE.csv')
 
 # Concentration and one variable
-data02=Scenary.scene2(dfx_train,historyStep,'MPU4_KW','EXH_CYL_GAS_TEMP_AVE')
+data02=CreateScenary.stage2(dfx_train,history,'MPU4_KW','EXH_CYL_GAS_TEMP_AVE')
 data02.to_csv('../Scenaries/EXH_CYL_GAS_TEMP_AVE/MPU4_KW VS EXH_CYL_GAS_TEMP_AVE.csv')
 
-data02=Scenary.scene2(dfx_train,historyStep,'WINDING_TEMP_AVE','EXH_CYL_GAS_TEMP_AVE')
+data02=CreateScenary.stage2(dfx_train,history,'WINDING_TEMP_AVE','EXH_CYL_GAS_TEMP_AVE')
 data02.to_csv('../Scenaries/EXH_CYL_GAS_TEMP_AVE/WINDING_TEMP_AVE VS EXH_CYL_GAS_TEMP_AVE.csv')
 
-data02=Scenary.scene2(dfx_train,historyStep,'EXH_INL_GAS_TEMP_AVE','EXH_CYL_GAS_TEMP_AVE')
+data02=CreateScenary.stage2(dfx_train,history,'EXH_INL_GAS_TEMP_AVE','EXH_CYL_GAS_TEMP_AVE')
 data02.to_csv('../Scenaries/EXH_CYL_GAS_TEMP_AVE/EXH_INL_GAS_TEMP_AVE VS EXH_CYL_GAS_TEMP_AVE.csv')
 
 
 #Concentration and two variables
-data03=Scenary.scene3(dfx_train,historyStep,'EXH_CYL_GAS_TEMP_AVE','WINDING_TEMP_AVE','EXH_CYL_GAS_TEMP_AVE')
+data03=CreateScenary.stage3(dfx_train,history,'EXH_CYL_GAS_TEMP_AVE','WINDING_TEMP_AVE','EXH_CYL_GAS_TEMP_AVE')
 data03.to_csv('../Scenaries/EXH_CYL_GAS_TEMP_AVE/EXH_CYL_GAS_TEMP_AVE WINDING_TEMP_AVE VS EXH_CYL_GAS_TEMP_AVE.csv')
 
-data03=Scenary.scene3(dfx_train,historyStep,'MPU4_KW','EXH_INL_GAS_TEMP_AVE','EXH_CYL_GAS_TEMP_AVE')
+data03=CreateScenary.stage3(dfx_train,history,'MPU4_KW','EXH_INL_GAS_TEMP_AVE','EXH_CYL_GAS_TEMP_AVE')
 data03.to_csv('../Scenaries/EXH_CYL_GAS_TEMP_AVE/MPU4_KW EXH_INL_GAS_TEMP_AVE VS EXH_CYL_GAS_TEMP_AVE.csv')
 
 #Concentration and three varaibles
-data04=Scenary.scene4(dfx_train,historyStep,'EXH_INL_GAS_TEMP_AVE','MPU4_KW','WINDING_TEMP_AVE','EXH_CYL_GAS_TEMP_AVE')
+data04=CreateScenary.stage4(dfx_train,history,'EXH_INL_GAS_TEMP_AVE','MPU4_KW','WINDING_TEMP_AVE','EXH_CYL_GAS_TEMP_AVE')
 data04.to_csv('../Scenaries/EXH_CYL_GAS_TEMP_AVE/EXH_INL_GAS_TEMP_AVE MPU4_KW WINDING_TEMP_AVE VS EXH_CYL_GAS_TEMP_AVE.csv')
 
 
 
 
 
-data01=Scenary.scene1(dfx_train,historyStep,'WINDING_TEMP_AVE')
+data01=CreateScenary.stage1(dfx_train,history,'WINDING_TEMP_AVE')
 data01.to_csv('../Scenaries/WINDING_TEMP_AVE/WINDING_TEMP_AVE.csv')
 
 # Concentration and one variable
-data02=Scenary.scene2(dfx_train,historyStep,'MPU4_KW','WINDING_TEMP_AVE')
+data02=CreateScenary.stage2(dfx_train,history,'MPU4_KW','WINDING_TEMP_AVE')
 data02.to_csv('../Scenaries/WINDING_TEMP_AVE/MPU4_KW VS WINDING_TEMP_AVE.csv')
 
-data02=Scenary.scene2(dfx_train,historyStep,'EXH_CYL_GAS_TEMP_AVE','WINDING_TEMP_AVE')
+data02=CreateScenary.stage2(dfx_train,history,'EXH_CYL_GAS_TEMP_AVE','WINDING_TEMP_AVE')
 data02.to_csv('../Scenaries/WINDING_TEMP_AVE/EXH_CYL_GAS_TEMP_AVE VS WINDING_TEMP_AVE.csv')
 
-data02=Scenary.scene2(dfx_train,historyStep,'EXH_INL_GAS_TEMP_AVE','WINDING_TEMP_AVE')
+data02=CreateScenary.stage2(dfx_train,history,'EXH_INL_GAS_TEMP_AVE','WINDING_TEMP_AVE')
 data02.to_csv('../Scenaries/WINDING_TEMP_AVE/EXH_INL_GAS_TEMP_AVE VS WINDING_TEMP_AVE.csv')
 
 
 #Concentration and two variables
-data03=Scenary.scene3(dfx_train,historyStep,'EXH_CYL_GAS_TEMP_AVE','MPU4_KW','WINDING_TEMP_AVE')
+data03=CreateScenary.stage3(dfx_train,history,'EXH_CYL_GAS_TEMP_AVE','MPU4_KW','WINDING_TEMP_AVE')
 data03.to_csv('../Scenaries/WINDING_TEMP_AVE/EXH_CYL_GAS_TEMP_AVE MPU4_KW VS WINDING_TEMP_AVE.csv')
 
-data03=Scenary.scene3(dfx_train,historyStep,'EXH_CYL_GAS_TEMP_AVE','EXH_INL_GAS_TEMP_AVE','WINDING_TEMP_AVE')
+data03=CreateScenary.stage3(dfx_train,history,'EXH_CYL_GAS_TEMP_AVE','EXH_INL_GAS_TEMP_AVE','WINDING_TEMP_AVE')
 data03.to_csv('../Scenaries/WINDING_TEMP_AVE/EXH_CYL_GAS_TEMP_AVE EXH_INL_GAS_TEMP_AVE VS WINDING_TEMP_AVE.csv')
 
 #Concentration and three varaibles
-data04=Scenary.scene4(dfx_train,historyStep,'EXH_INL_GAS_TEMP_AVE','MPU4_KW','EXH_CYL_GAS_TEMP_AVE','WINDING_TEMP_AVE')
+data04=CreateScenary.stage4(dfx_train,history,'EXH_INL_GAS_TEMP_AVE','MPU4_KW','EXH_CYL_GAS_TEMP_AVE','WINDING_TEMP_AVE')
 data04.to_csv('../Scenaries/WINDING_TEMP_AVE/EXH_INL_GAS_TEMP_AVE MPU4_KW EXH_CYL_GAS_TEMP_AVE VS WINDING_TEMP_AVE.csv')
 
 
-data01=Scenary.scene1(dfx_train,historyStep,'EXH_INL_GAS_TEMP_AVE')
+data01=CreateScenary.stage1(dfx_train,history,'EXH_INL_GAS_TEMP_AVE')
 data01.to_csv('../Scenaries/EXH_INL_GAS_TEMP_AVE/EXH_INL_GAS_TEMP_AVE.csv')
 
 # Concentration and one variable
-data02=Scenary.scene2(dfx_train,historyStep,'MPU4_KW','EXH_INL_GAS_TEMP_AVE')
+data02=CreateScenary.stage2(dfx_train,history,'MPU4_KW','EXH_INL_GAS_TEMP_AVE')
 data02.to_csv('../Scenaries/EXH_INL_GAS_TEMP_AVE/MPU4_KW VS EXH_INL_GAS_TEMP_AVE.csv')
 
-data02=Scenary.scene2(dfx_train,historyStep,'EXH_CYL_GAS_TEMP_AVE','EXH_INL_GAS_TEMP_AVE')
+data02=CreateScenary.stage2(dfx_train,history,'EXH_CYL_GAS_TEMP_AVE','EXH_INL_GAS_TEMP_AVE')
 data02.to_csv('../Scenaries/EXH_INL_GAS_TEMP_AVE/EXH_CYL_GAS_TEMP_AVE VS EXH_INL_GAS_TEMP_AVE.csv')
 
-data02=Scenary.scene2(dfx_train,historyStep,'WINDING_TEMP_AVE','EXH_INL_GAS_TEMP_AVE')
+data02=CreateScenary.stage2(dfx_train,history,'WINDING_TEMP_AVE','EXH_INL_GAS_TEMP_AVE')
 data02.to_csv('../Scenaries/EXH_INL_GAS_TEMP_AVE/WINDING_TEMP_AVE VS EXH_INL_GAS_TEMP_AVE.csv')
 
 
 #Concentration and two variables
-data03=Scenary.scene3(dfx_train,historyStep,'EXH_CYL_GAS_TEMP_AVE','MPU4_KW','EXH_INL_GAS_TEMP_AVE')
+data03=CreateScenary.stage3(dfx_train,history,'EXH_CYL_GAS_TEMP_AVE','MPU4_KW','EXH_INL_GAS_TEMP_AVE')
 data03.to_csv('../Scenaries/EXH_INL_GAS_TEMP_AVE/EXH_CYL_GAS_TEMP_AVE MPU4_KW VS EXH_INL_GAS_TEMP_AVE.csv')
 
-data03=Scenary.scene3(dfx_train,historyStep,'EXH_CYL_GAS_TEMP_AVE','WINDING_TEMP_AVE','EXH_INL_GAS_TEMP_AVE')
+data03=CreateScenary.stage3(dfx_train,history,'EXH_CYL_GAS_TEMP_AVE','WINDING_TEMP_AVE','EXH_INL_GAS_TEMP_AVE')
 data03.to_csv('../Scenaries/EXH_INL_GAS_TEMP_AVE/EXH_CYL_GAS_TEMP_AVE WINDING_TEMP_AVE VS EXH_INL_GAS_TEMP_AVE.csv')
 
 #Concentration and three varaibles
-data04=Scenary.scene4(dfx_train,historyStep,'WINDING_TEMP_AVE','MPU4_KW','EXH_CYL_GAS_TEMP_AVE','EXH_INL_GAS_TEMP_AVE')
+data04=CreateScenary.stage4(dfx_train,history,'WINDING_TEMP_AVE','MPU4_KW','EXH_CYL_GAS_TEMP_AVE','EXH_INL_GAS_TEMP_AVE')
 data04.to_csv('../Scenaries/EXH_INL_GAS_TEMP_AVE/WINDING_TEMP_AVE MPU4_KW EXH_CYL_GAS_TEMP_AVE VS EXH_INL_GAS_TEMP_AVE.csv')
 
 
 
-print('Successfully Generated')
+print('Generated Successfully :)')
 ##############################################################################
